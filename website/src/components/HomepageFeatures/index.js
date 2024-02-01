bd3a6fd6f3ee82f0871e4e54b49d6684574f6afb
nitya <nitya@users.noreply.github.com>
2024-02-01 07:37:15 -0500

    Update Landing Page With Partners

--- website/src/components/HomepageFeatures/index.js
+++ website/src/components/HomepageFeatures/index.js
@@ -1,6 +1,60 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
+import Image from '@theme/IdealImage';
+
+import zindi from './../../../static/img/partners/zindi.png';
+import mlsa from './../../../static/img/partners/mlsa.png';
+import startups from './../../../static/img/partners/startups.png';
+
+const PartnersList = [
+  {
+    title: 'Zindi Africa',
+    logo: zindi,
+    link: 'https://zindi.africa',
+    description: (
+      <>
+        Zindi hosts the largest community of African data practitioners, working to solve the world’s most pressing challenges using machine learning and AI. We want to showcase African data talent to the world.
+      </>
+    ),
+  },
+  {
+    title: 'Microsoft Learn Student Ambassadors',
+    logo: mlsa,
+    link: 'https://mvp.microsoft.com/studentambassadors',
+    description: (
+      <>
+        Join our global community of students passionate about building AI-driven solutions with Microsoft technology. Accelerate innovation and grow the skills for greater impact in projects and communities that matter to you.
+      </>
+    ),
+  },
+  {
+    title: 'Microsoft For Startups',
+    logo: startups,
+    link: 'https://www.microsoft.com/startups',
+    description: (
+      <>
+        Microsoft for Startups Founders Hub helps startups radically accelerate innovation by providing access to industry-leading AI services, expert guidance, and the essential technology needed to build a future-proofed startup.
+      </>
+    ),
+  },
+];
+
+function Partner({ logo, title, link, description }) {
+  return (
+    <div className={clsx('col col--4', styles.contentItem)}>
+      <div className="avatar avatar--vertical">
+        <Image className="avatar__photo avatar__photo--xl" role="img" img={logo} />
+        <div className="avatar__intro margin-vert--md">
+          <h3><a href={link}>{title}</a></h3>
+          <div className="avatar__subtitle">
+            {description}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 const FeatureList = [
   {
@@ -11,7 +65,7 @@ const FeatureList = [
     topiclink: 'https://aka.ms/rai-hub/dashboard-workshop',
     description: (
       <>
-        This hands-on lesson teaches how to debug and mitigate a ML model issues using error analysis, data analysis, model explainability, model performance and fairness assessment. 
+        This hands-on lesson teaches how to debug and mitigate a ML model issues using error analysis, data analysis, model explainability, model performance and fairness assessment.
       </>
     ),
   },
@@ -63,7 +117,6 @@ function Feature({ Svg, title, description, topic, topiclink, link }) {
   );
 }
 
-
 export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
@@ -71,6 +124,13 @@ export default function HomepageFeatures() {
         <div className="row">
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
+          ))}
+        </div>
+        <hr />
+        <h2 className="text--center"> Our Community Partners </h2>
+        <div className="row">
+          {PartnersList.map((props, idx) => (
+            <Partner key={idx} {...props} />
           ))}
         </div>
       </div>
